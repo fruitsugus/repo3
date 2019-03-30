68c4f6da5930cde8458b77c0a4fddf966582e251
fruisugus <530734281@qq.com>
2019-03-30 11:47:49 +0800

    添加test工程至git

---
+++ idea-git-test/src/com/itheima/test/GitTest.java
@@ -0,0 +1,9 @@
+package com.itheima.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+    }
+}
